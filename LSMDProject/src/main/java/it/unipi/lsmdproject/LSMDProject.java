eab089d41d290ff5f7ab650977cc8df018ebae02
NicolaBerti98 <nicola_247@libero.it>
2024-10-16 15:23:12 +0200

    dios

--- LSMDProject/src/main/java/it/unipi/lsmdproject/LSMDProject.java
+++ LSMDProject/src/main/java/it/unipi/lsmdproject/LSMDProject.java
@@ -15,10 +15,37 @@ import javafx.stage.Stage;
  *
  * @author erni
  */
-public class LSMDProject {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class LSMDProject extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Inizializzazione del pulsante
+        Button btn = new Button();
+        btn.setText("Cliccami!");
+
+        // Stampa del testo del pulsante - ora funziona correttamente all'interno del thread JavaFX
+        System.out.println("Testo del pulsante: " + btn.getText());
+
+        // Configurazione del layout
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        // Creazione della scena
+        Scene scene = new Scene(root, 300, 250);
+
+        // Imposta la finestra principale
+        primaryStage.setTitle("Verifica JavaFX");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
-        Button btn = new Button();
-        StackPane root = new StackPane();
+        launch(args);  // Avvio dell'applicazione JavaFX
     }
 }
