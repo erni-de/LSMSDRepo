acd0b8160edf7c740248172c90e014d176b9fce7
erni-de <erni.deliallisi00@gmail.com>
2024-10-16 14:36:29 +0200

    First Commit

---
+++ LSMDProject/src/main/java/it/unipi/lsmdproject/LSMDProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package it.unipi.lsmdproject;
+
+/**
+ *
+ * @author erni
+ */
+public class LSMDProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
